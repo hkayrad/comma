5b07e42dceb8b3ab33d54bb8e78ea76f58f67c58
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-11-19 16:21:50 +0300

    fix: remove authorization checks from admin page

--- client/src/layout/admin/Admin.tsx
+++ client/src/layout/admin/Admin.tsx
@@ -1,10 +1,3 @@
-import { SystemAdminOnly } from "../auth/RoleGuard";
-import NotFound from "../NotFound";
-
 export default function Admin() {
-  return (
-    <SystemAdminOnly fallback={<NotFound />}>
-      <div className="p-4">Admin</div>
-    </SystemAdminOnly>
-  );
+  return <div className="p-4">Admin</div>;
 }
